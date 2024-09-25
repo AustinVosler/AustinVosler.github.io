c7c9d55d893d3098fe906e2e674ae57ffa8870e7
AustinVosler <austinpvosler@gmail.com>
2024-09-25 13:07:59 -0400

    update test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,14 @@
 export default function Home() {
   return (
     <main className="min-h-screen p-4 xl:p-24 bg-black w-full">
-      hi
+      <div className="flex flex-col xl:flex-row w-full h-full justify-between">
+        <div className=" text-lg font-[family-name:var(--font-geist-mono)]">
+          Hello
+        </div>
+        <div>
+          Hello 2
+        </div>
+      </div>
     </main>
   );
 }
