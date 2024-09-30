15e4f5148f6eafa9746fd27ea2c19cc301a03fc3
AustinVosler <austinpvosler@gmail.com>
2024-09-30 16:08:28 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,20 +12,25 @@ export default function Home() {
           <div className="text-2xl pb-4">
             I am a 3rd-year member of the Burnett Honors College at the University of Central Florida, where I am studying Computer Science.
           </div>
-          <div className="text-2xl ">
+          <div className="text-2xl pb-4 ">
             As a passionate scholar, I am dedicated to learning everything I can to be able to better the world.
             Whether it is solving problems as a programmer, or adding to the world&#39;s beauty as a musician, it is important
             for me to find ways to both challenge myself and positively impact my community around me. 
           </div>
+          <Link href="/projects/project1">
+            <button className="bg-sky-700 hover:bg-neutral-700 text-white font-bold py-2 px-4 rounded font-[family-name:var(--font-geist-mono)]">
+              Button
+            </button>
+          </Link>
         </div>
           <div className="w-full h-full flex justify-center">
             <Image 
               className="rounded-full aspect-square object-cover"
               src="/headshot.jpg"
-              alt="headshot of Austin Vosler"
+              alt="headshot of Austin   Vosler"
               // fill
-              width="650"
-              height="650"
+              width="500"
+              height="500"
             />
           </div>
       </div>
